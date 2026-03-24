8ca6a84926ed74e61231ddd6180411ce8a127c00
Alexis Dariel <darixsango@hotmail.com>
2026-03-23 22:18:00 -0500

    creacion dto en la logica del negocio

---
+++ src/domain/archivo/upload-file.dto.ts
@@ -0,0 +1,21 @@
+
+class UploadFileDto { 
+    
+    
+    public mimetype: string   
+    public size: number   
+    public filename: string   
+    
+    constructor(mimetype: string, filename: string, size: number) {
+        this.mimetype = mimetype
+        this.filename = filename
+        this.size = size
+    }
+
+    static create(mimetype: string, filename: string, size: number) {
+        if (!mimetype || !filename || !size) {
+            return [new Error('Faltan datos'), null]
+        }
+        return [null, new UploadFileDto(mimetype, filename, size)]
+    }
+}
