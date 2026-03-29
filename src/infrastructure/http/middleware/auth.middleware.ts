cc467b17aed1da738bd5a5c96fa586a510f9eeab
Alexis Dariel <darixsango@hotmail.com>
2026-03-29 18:01:52 -0500

    routes y middleware

--- src/infrastructure/http/middleware/auth.middleware.ts
+++ src/infrastructure/http/middleware/auth.middleware.ts
@@ -1,1 +1,16 @@
 import jwt from 'jsonwebtoken'
+import type {Request, Response, NextFunction } from 'express'
+export class authMiddleware{
+    static middleware = async (req: Request, res:Response, next: NextFunction)=>{
+        const authHeader = req.headers.authorization
+        if (!authHeader) return res.status(401).json({ message: 'No autorizado' })
+        const token = authHeader.split(' ')[1]  
+        if (!token) return res.status(401).json({ message: 'No autorizado' })
+        try {
+            jwt.verify(token, process.env.JWT_SECRET || 'secreto')
+            next()
+        } catch {
+            return res.status(401).json({ message: 'Token inválido' })
+        }
+    }
+}
--- src/infrastructure/http/routes/archivos.routes.ts
+++ src/infrastructure/http/routes/archivos.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import {ArchivosController} from '../controllers/archivos.controller.js'
 import { upload } from '../middleware/multer.middleware.js'
 import {ValidateFile} from '../middleware/validarArchivo.middleware.js'
+import {authMiddleware} from '../middleware/auth.middleware.js'
 export class AppRoutes{
 
     static get routes():Router{
